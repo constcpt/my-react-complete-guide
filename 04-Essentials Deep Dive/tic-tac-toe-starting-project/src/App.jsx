74d0a9d8f4edaf45dcc90c223b0502476dbf4510
constcpt <51440152+constcpt@users.noreply.github.com>
2023-12-10 03:44:56 +1100

    Note: Component Instance Work in Isolation!

--- 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/App.jsx
+++ 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
+          {/* totally different isolated instances are created which only use the same logic but they then use it on their own. */}
           <Player name="Player 1" symbol="X" />
           <Player name="Player 2" symbol="O" />
         </ol>
--- 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
+  /* Component Instance Work in Isolation
+    If the state in this first player component instance here changes, the second player component instance does not care about that at all. It doesn't even know about that.*/
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
     setIsEditing(true);
