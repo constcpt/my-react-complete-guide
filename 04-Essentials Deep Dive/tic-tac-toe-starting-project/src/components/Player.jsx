4880e9b8302348aa9af5b3e7d38eaedd2f427f1c
constcpt <51440152+constcpt@users.noreply.github.com>
2023-12-10 22:48:43 +1100

    Conditional Content & A bad Way Of Updating State

--- 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/components/Player.jsx
@@ -5,21 +5,22 @@ export default function Player({ name, symbol }) {
     If the state in this first player component instance here changes, the second player component instance does not care about that at all. It doesn't even know about that.*/
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
   }
 
   return (
     <li>
       <span className="player">
-        {isEditing ? (
-          <input type="text" required />
-        ) : (
-          <span className="player-name">{name}</span>
-        )}
-
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
