cd0a0f27dfe74d3e191113609ce5abe8f8a41e17
constcpt <51440152+constcpt@users.noreply.github.com>
2023-12-11 01:59:55 +1100

    User Input & Two-Way-Binding

--- 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/App.jsx
+++ 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
       <div id="game-container">
         <ol id="players">
           {/* totally different isolated instances are created which only use the same logic but they then use it on their own. */}
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
         Game board
       </div>
--- 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,24 +1,33 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   /* Component Instance Work in Isolation
     If the state in this first player component instance here changes, the second player component instance does not care about that at all. It doesn't even know about that.*/
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
+
   function handleEditClick() {
     // setIsEditing(!isEditing); // do not update the state based on the previous state like this, use the function form instead.
     setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
-
-  if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+  function handleChange(event) {
+    setPlayerName(event.target.value); // event.targert refers to the element that did trigger the event, in this case the input element. And this input element has a value property which holds the current value of the input.
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
