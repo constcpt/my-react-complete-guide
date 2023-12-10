859dbc354fd7a3e39d78a87dab45b2bdfc9fe8a2
constcpt <51440152+constcpt@users.noreply.github.com>
2023-12-11 00:22:16 +1100

    Best Practice: Updating State Based On Old State Correctly by using Functio form of state update

--- 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04-Essentials Deep Dive/tic-tac-toe-starting-project/src/components/Player.jsx
@@ -5,7 +5,8 @@ export default function Player({ name, symbol }) {
     If the state in this first player component instance here changes, the second player component instance does not care about that at all. It doesn't even know about that.*/
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
-    setIsEditing(!isEditing);
+    // setIsEditing(!isEditing); // do not update the state based on the previous state like this, use the function form instead.
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
